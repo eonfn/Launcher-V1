da325ec5e62a06b1794ec6b900abc099bcfb6c4c
zinx <63958372+zinx-YT@users.noreply.github.com>
2023-07-04 21:46:43 +0100

    Eon Launcher 0.1

---
+++ WpfApp6/Pages/Home.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using WpfApp6.Services;
+using WpfApp6.Services.Launch;
+
+namespace WpfApp6.Pages
+{
+	/// <summary>
+	/// Interaction logic for Home.xaml
+	/// </summary>
+	public partial class Home : UserControl
+	{
+		public Home()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				string Path69 = UpdateINI.ReadValue("Auth", "Path");
+				if (Path69 != "NONE") // NONE THE BEST RESPONSE!
+				{
+					//MessageBox.Show(Path69);
+					if (File.Exists(System.IO.Path.Join(Path69, "FortniteGame\\Binaries\\Win64\\FortniteClient-Win64-Shipping.exe")))
+					{
+						if (UpdateINI.ReadValue("Auth", "Email") == "NONE" || UpdateINI.ReadValue("Auth", "Password") == "NONE")
+						{
+							MessageBox.Show("Please Add Your Eon Info In Settings");
+							return;
+						}
+						WebClient OMG = new WebClient();
+						OMG.DownloadFile("https://cdn.discordapp.com/attachments/1125117879763865642/1125490815372890183/EonCurl.dll", Path.Combine(Path69, "Engine\\Binaries\\ThirdParty\\NVIDIA\\NVaftermath\\Win64", "GFSDK_Aftermath_Lib.x64.dll"));
+						//AntiCheat.Start(Path69);
+						PSBasics.Start(Path69, "-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -noeac -fromfl=be -fltoken=h1cdhchd10150221h130eB56 -skippatchcheck", UpdateINI.ReadValue("Auth", "Email"), UpdateINI.ReadValue("Auth", "Password"));
+						FakeAC.Start(Path69, "FortniteClient-Win64-Shipping_BE.exe", $"-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -noeac -fromfl=be -fltoken=h1cdhchd10150221h130eB56 -skippatchcheck", "r");
+						FakeAC.Start(Path69, "FortniteLauncher.exe", $"-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -noeac -fromfl=be -fltoken=h1cdhchd10150221h130eB56 -skippatchcheck", "dsf");
+						PSBasics._FortniteProcess.WaitForExit();
+						try
+						{
+							FakeAC._FNLauncherProcess.Close();
+							FakeAC._FNAntiCheatProcess.Close();
+						}
+						catch (Exception ex)
+						{
+							MessageBox.Show("There Been A Error Closing");
+						}
+
+
+
+						//Injector.Start(PSBasics._FortniteProcess.Id, Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "EonCurl.dll"));
+					}
+					else
+					{
+						MessageBox.Show("Please Add Your Eon Info In Settings"); // INV
+					}
+				}
+			}catch (Exception ex)
+			{
+				MessageBox.Show("ERROR");
+			}
+			
+		}
+	}
+}
---
+++ WpfApp6/Services/UpdateINI.cs
@@ -0,0 +1,63 @@
+﻿using IniParser;
+using IniParser.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Windows.Controls.Map;
+
+namespace WpfApp6.Services
+{
+	// Code Might Be Bad Never Really Touched INI Files
+	public static class UpdateINI
+	{
+		public static void WriteToConfig(string SectionName, string PathKey, string NewValue)
+		{
+			string BaseFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+			string DataFolder = Path.Combine(BaseFolder, "Eon");
+			Directory.CreateDirectory(DataFolder);
+			string FilePath = Path.Combine(DataFolder, "Settings.ini");
+
+
+			FileIniDataParser parser = new FileIniDataParser(); // nEW!
+
+			IniData iniData;
+			if (File.Exists(FilePath))
+			{
+				iniData = parser.ReadFile(FilePath);
+			}
+			else
+			{
+				iniData = new IniData();
+			}
+
+			// This Updates The Current Values - IG!?!?!?!?!?!??!?!//
+
+			//IniData iniData = parser.ReadFile(FilePath);
+			iniData[SectionName][PathKey] = NewValue;
+			parser.WriteFile(FilePath, iniData, null);
+		}
+
+		public static string ReadValue(string SectionName, string PathKey) // NO T writing !
+		{
+			string BaseFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+			string DataFolder = Path.Combine(BaseFolder, "Eon");
+			string FilePath = Path.Combine(DataFolder, "Settings.ini");
+
+			FileIniDataParser parser = new FileIniDataParser();
+
+			if (File.Exists(FilePath))
+			{
+				IniData iniData = parser.ReadFile(FilePath);
+
+				return iniData[SectionName][PathKey];
+			}
+			else
+			{
+				return "NONE";
+			}
+		}
+	}
+}
